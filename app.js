2f22d40199c39b124cc858157fe8430a507fd680
Kristjan T <43129577+KristjanTreimann@users.noreply.github.com>
2020-07-29 14:41:41 +0300

    game board logic

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let width = 10
   let bombAmount = 20
   let squares = []
+  let numbers = []
 
   //create Board
   function createBoard() {
@@ -19,23 +20,45 @@ document.addEventListener('DOMContentLoaded', () => {
       square.classList.add(shuffledArray[i]) // add 'bomb' & 'valid' as classes to squares
       grid.appendChild(square)
       squares.push(square)
+
+      square.addEventListener('click', function () {
+        console.log(square)
+      })
     }
 
     //add numbers
     for (let i = 0; i < squares.length; i++) {
       let total = 0
-      let neBomb = 0
+      let test = 0
       const isLeftEdge = i % width === 0 // divides with 10
       const isRightEdge = i === width - 1 // ends with 9
       if (squares[i].classList.contains('valid')) {
-        // not 0 && not on left edge && square left from my square contains bomb -> add 1 to total
+        // 1-LEFT contains a bomb
+        // not 0 && not on left edge && 1-left from my square contains bomb -> add 1 to total
         if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb')) total++
-        /* squares[i].setAttribute('leftBomb', total)
+
+        // 1 UP-RIGHT contains a bomb
+        if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains('bomb')) total++
+        /* squares[i].setAttribute('total', total) 
         console.log(squares[i]) */
-        // if north east contains a bomb -> e.g (self[18])(bomb[9])
-        if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains('bomb')) neBomb++
-        /* squares[i].setAttribute('northEastBomb', neBomb)
-		console.log(squares[i]) */
+
+        // 1 UP contains a bomb
+        if (i > 10 && squares[i - width].classList.contains('bomb')) total++
+
+        // 1 UP-LEFT contains a bomb
+        if (i > 11 && !isLeftEdge && squares[i - 1 - width].classList.contains('bomb')) total++
+
+        // 1 DOWN contains a bomb
+        if (i < 98 && !isRightEdge && squares[i + 1].classList.contains('bomb')) total++
+
+        // 1 DOWN-LEFT
+        if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) total++
+
+        // 1 DOWN-RIGHT
+        if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('bomb')) total++
+
+        // 1 DOWN
+        if (i < 89 && squares[i + width].classList.contains('bomb')) total++
       }
     }
   }
